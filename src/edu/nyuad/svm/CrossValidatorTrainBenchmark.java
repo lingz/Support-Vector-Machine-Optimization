e2eb57260857eeefdc20a9832b2230fee41b1000
Moiri Gamboni <Moiri Gamboni>
2013-11-06 11:55:53 +0400

    added filters to caliper

--- src/edu/nyuad/svm/CrossValidatorTrainBenchmark.java
+++ src/edu/nyuad/svm/CrossValidatorTrainBenchmark.java
@@ -40,6 +40,133 @@ public class CrossValidatorTrainBenchmark extends Benchmark{
 		"8",
 		"9",
 	}) int fold;
+	
+	@Param Filter filter;
+	public enum Filter{
+		NONE{
+
+			@Override
+			public void setFilter(DataFilters dataset) {
+				dataset.noFilter();
+				
+			}
+			
+		},
+		PERCENTAGE_10{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.percentageFilter(10);
+			}
+			
+		},
+		PERCENTAGE_20{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.percentageFilter(20);
+			}
+			
+		},
+		PERCENTAGE_30{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.percentageFilter(30);
+			}
+			
+		},
+		PERCENTAGE_40{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.percentageFilter(40);
+			}
+			
+		},
+		PERCENTAGE_50{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.percentageFilter(50);
+			}
+			
+		},
+		PERCENTAGE_60{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.percentageFilter(60);
+			}
+			
+		},
+		PERCENTAGE_70{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.percentageFilter(70);
+			}
+			
+		},
+		PERCENTAGE_80{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.percentageFilter(80);
+			}
+			
+		},
+		PERCENTAGE_90{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.percentageFilter(90);
+			}
+			
+		},
+		GAUSSIAN{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.gaussianFilter();
+				
+			}
+			
+		},
+		WILSON{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.wilsonFilter();
+			}
+			
+		},
+		WILSON_AND_GAUSSIAN{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.wilsonAndGaussianFilter();
+			}
+			
+		},
+		WILSON_CONDENSING{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.wilsonCondensationFilter();
+			}
+			
+		},
+		WILSON_AND_WILSON_CONDENSING{
+
+			@Override
+			public void setFilter(DataFilters dataset) throws Exception {
+				dataset.wilsonAndWilsonCondensationFilter();
+			}
+			
+		};
+		abstract public void setFilter(DataFilters dataset) throws Exception;
+	}
 	 	
 	
 	private Instances train;
@@ -52,7 +179,7 @@ public class CrossValidatorTrainBenchmark extends Benchmark{
         match.matches();
         String filename = match.group(1);
         DataFilters dataset = new DataFilters(filepath);
-        dataset.noFilter();
+        filter.setFilter(dataset);
         CrossValidator cv = new CrossValidator(type, dataset);
         train = cv.data.trainCV(10, fold);
         test = cv.data.testCV(10, fold);
